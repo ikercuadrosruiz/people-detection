6c0e10374056b47ed0aa7961fe6337a0940ad505
ikercuadrosruiz <ikercuadrosruiz0@gmail.com>
2024-10-10 13:40:31 +0200

    Actualización main.py

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ import gc
 import shutil
 import time
 import glob
+
+# Main 
+import random
+import numpy as np
+import pandas as pd
+import json
+import cv2
+from tqdm import tqdm
+tqdm.pandas()
+from spicy.io import loadmat
